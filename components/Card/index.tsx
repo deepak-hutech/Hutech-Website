50b836f945da42e975c45f5ef843a69405e5c8fe
Swati Dewangan <swatidewangan@Swatis-MacBook-Air.local>
2022-01-03 00:02:35 +0530

    responsive for all page

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -557,7 +557,16 @@ const Card: NextPage = (props: any) => {
     default:
       return (
         <div className={companyStyles.c_cards}>
-          <img src={`${baseUrl}${(images[0].url)as any}`} className={styles.hello} />
+          {/* <img src={`${baseUrl}${images[0].url}`} className={styles.hello} /> */}
+          <Image
+              loader={() => myLoader((baseUrl + images[0].url) as any)}
+              src={`${baseUrl}${images[0].url}`}
+              placeholder="blur"
+              blurDataURL={baseUrl + images.url}
+              width={"100%"}
+              height={"70%"}
+              className={styles.hello}
+            />
           <p>{props.heading}</p>
         </div>
       );
