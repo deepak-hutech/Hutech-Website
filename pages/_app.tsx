a47a6927c41202dd72912cd7788f6db6909a1721
virinchi-hutech <virinchi@hutechsolutions.com>
2022-01-28 17:05:22 +0530

    ADD Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,35 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "nextjs-breadcrumbs/dist/index.css";
+import { useEffect } from "react";
 import "../styles/globals.css";
 import strings from "./../public/strings.json";
 
 const { baseUrl } = strings;
 function MyApp(props: any) {
   const { Component, pageProps } = props;
-  return <Component {...pageProps} />;
+  useEffect(()=>{
+    const _global:any = window;
+    console.log("###GTMID",process.env);
+    
+    _global['gtm_id'] = process.env.GTM_ID || "GTM-NQK9XJ2";
+		const scriptGtm = document.createElement('script');
+		scriptGtm.type = 'text/javascript';
+		scriptGtm.src = '/static/googletagmanager.js';
+		scriptGtm.defer = true;
+		document.head.appendChild(scriptGtm);
+  })
+  return <>
+  <noscript>
+  <iframe
+    src={`https://www.googletagmanager.com/ns.html?id=${process.env.GTM_ID}`}
+    height="0"
+    width="0"
+    style={{ display: 'none', visibility: 'hidden' }}
+    title="gtm"
+  />
+</noscript>
+<Component {...pageProps} />
+</>
 }
 
 export default MyApp;
